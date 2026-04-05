ef40808b5ebbf691df977db277851a7fe3e0e8b6
Antoine Libert <contact@antoine-libert.com>
2026-04-05 16:10:30 +0200

    fix: prisma deps

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "🚀 Starting container..."
 
 echo "🗄️ Running Prisma migrations..."
-npx prisma migrate deploy
+node ./node_modules/prisma/build/index.js migrate deploy
 
 echo "✅ Migrations applied"
 
